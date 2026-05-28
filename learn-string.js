6af425d00bf9a862c67f4fe4fcaf378fbe7a5733
Kavanaca <kavanakavanaklr123@gmail.com>
2026-05-28 09:55:48 +0530

    april class

---
+++ learn-string.js
@@ -0,0 +1,8 @@
+let str = "Banglore";
+console.log(str);
+console.log(str.length);
+console.log(str.toLocaleLowerCase());
+console.log(str.toLocaleUpperCase());
+console.log(str.charAt(0));
+console.log(str.charAt(1));
+console.log(str.repeat(" " + 3));
